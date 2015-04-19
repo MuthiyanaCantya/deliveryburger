502d66a3b84134ef094b4bc0ce742ccef800aef3
Muthiyana Cantya Puspita <13.7767@stis.ac.id>
2015-04-19 18:52:33 +0700

    Update alamat.java

--- alamat.java
+++ alamat.java
@@ -1,18 +1,23 @@
 public class Alamat {
+    
     private String jalan;
     private String kecamatan;
     private String kota;
+    
     public Alamat(String jalan, String kec, String kota) {
         this.jalan = jalan;
         kecamatan = kec;
         this.kota = kota;
     }
+    
     public String getJalan() {
         return jalan;
     }
+    
     public String getKecamatan() {
         return kecamatan;
     }
+    
     public String getKota() {
         return kota;
     }
